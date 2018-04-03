c112df957d52bb37eb2a3234e61cc5dcc7afb4ff
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-03 11:43:18 -0700

    vs995: Override all fingerprints
    Change-Id: Ie3b4c22477377e26058c66104c2c33d19b6b1203

--- lineage.mk
+++ lineage.mk
@@ -11,5 +11,6 @@ PRODUCT_NAME := lineage_vs995
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v20" \
     PRODUCT_NAME="elsa_vzw_us" \
-    BUILD_FINGERPRINT="lge/elsa_vzw/elsa:7.0/NRD90M/1628409164d39:user/release-keys" \
     PRIVATE_BUILD_DESC="elsa_vzw-user 7.0 NRD90M 1628409164d39 release-keys"
+
+BUILD_FINGERPRINT := "lge/elsa_vzw/elsa:7.0/NRD90M/1628409164d39:user/release-keys"
