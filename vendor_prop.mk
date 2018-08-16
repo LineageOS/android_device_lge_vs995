c94a7fcebd375e439e1b5cccb326b59ac55a5a3f
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-16 10:23:39 -0700

    vs995: Move device specific props to vendor
    Change-Id: I5a4adf2a6922cc305d31e569dc32ff038cd92b59

---
+++ vendor_prop.mk
@@ -0,0 +1,10 @@
+#
+# Vendor Properties for Verizon LG V20
+#
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_network=10 \
+    telephony.lteOnCdmaDevice=1 \
+    ro.cdma.home.operator.alpha=Verizon \
+    ro.cdma.home.operator.numeric=311480
