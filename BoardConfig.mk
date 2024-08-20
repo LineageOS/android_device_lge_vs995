7e6f6bcbfa7e29207a920740181c529f578888b0
AShiningRay <genesis45.ppc@gmail.com>
2024-08-20 19:32:46 -0300

    vs995: Move to kernel config fragments
    Change-Id: I76c0f3a537cc8ebe8f6f2fff757d40c6bb3eb3fb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ DEVICE_PATH := device/lge/vs995
 TARGET_OTA_ASSERT_DEVICE := vs995,us996,elsa
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_vs995_defconfig
+TARGET_KERNEL_CONFIG := vendor/lge/vs995.config
 
 # inherit from the proprietary version
 include vendor/lge/vs995/BoardConfigVendor.mk
