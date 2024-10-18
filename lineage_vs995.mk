253c664f30e98392a80002d227e7d5a8c503a7fa
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    vs995: Adjust override prop key names
    Change-Id: Ib2f1cbfcc0f28c1eb3f3d0220aa5d2a4df1ae8e5

--- lineage_vs995.mk
+++ lineage_vs995.mk
@@ -32,9 +32,7 @@ PRODUCT_MODEL := LG-VS995
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="elsa" \
-    PRODUCT_DEVICE="elsa" \
-    PRODUCT_NAME="elsa_vzw_us" \
-    PRIVATE_BUILD_DESC="elsa_vzw-user 8.0.0 OPR1.170623.032 183141449275f release-keys"
-
-BUILD_FINGERPRINT := "lge/elsa_vzw/elsa:8.0.0/OPR1.170623.032/183141449275f:user/release-keys"
+    BuildDesc="elsa_vzw-user 8.0.0 OPR1.170623.032 183141449275f release-keys" \
+    BuildFingerprint="lge/elsa_vzw/elsa:8.0.0/OPR1.170623.032/183141449275f:user/release-keys" \
+    DeviceName="elsa_vzw_us" \
+    DeviceProduct="elsa"
