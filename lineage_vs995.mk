7db5fe65c7f96e6047aea5341a75d7e3b54d4bba
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-06 17:28:21 -0400

    vs995: Migrate from DeviceName -> System{Device,Name} to avoid OTA assert issues
    Change-Id: I5a2a667ed77082a380b132f42354218d41e10e34

--- lineage_vs995.mk
+++ lineage_vs995.mk
@@ -34,5 +34,6 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="elsa_vzw-user 8.0.0 OPR1.170623.032 183141449275f release-keys" \
     BuildFingerprint="lge/elsa_vzw/elsa:8.0.0/OPR1.170623.032/183141449275f:user/release-keys" \
-    DeviceName="elsa_vzw_us" \
+    SystemDevice="elsa_vzw_us" \
+    SystemName="elsa_vzw_us" \
     DeviceProduct="elsa"
